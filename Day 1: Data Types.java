bb29e49aba59a1797896ee1ecfe405a8942c1875
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-06 08:19:54 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        int j=scan.nextInt();
+        System.out.println(i+j);
+        double k=scan.nextDouble();
+        System.out.println(k+d);
+        scan.nextLine();
+        String sc=scan.nextLine();
+        System.out.println(s+sc);
+        
+        
+
+
+        scan.close();
+    }
+}
