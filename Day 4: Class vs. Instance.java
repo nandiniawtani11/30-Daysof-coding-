0c75a010ee2d9356e8ef52d8897875bee8704058
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-10 08:11:43 +0530

    Create Day 4: Class vs. Instance.java

---
+++ Day 4: Class vs. Instance.java
@@ -0,0 +1,27 @@
+  private int age;    
+  
+    public Person(int initialAge) {
+          if(initialAge>1)
+          this.age=initialAge;
+          else
+          {
+              this.age=0;
+              System.out.println("Age is not valid, setting age to 0.");   
+          }    
+    }
+    public void amIOld() {
+          if(age==0)
+          {
+              System.out.println("You are young.");
+              return;
+          }
+          else if(age<13)         
+        System.out.println("You are young.");
+        else if(age>=13 && age<18)
+        System.out.println("You are a teenager.");
+        else
+        System.out.println("You are old.");        
+    }
+    public void yearPasses() {
+          age=age+1;
+    }
